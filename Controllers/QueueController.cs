2c62a51ba52fb44a4e9d4d0d635aa65959b6daca
ajaysbelgur <ajaysbelgur@gmail.com>
2022-02-01 09:50:55 +0530

    Add project files.

---
+++ Controllers/QueueController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QueueController : ControllerBase
+    {
+        private static string connectionString = "";
+        private static string queue = "";
+        private IQueueClient queueClient;
+        public QueueController()
+        {
+            queueClient = new QueueClient(connectionString, queue);
+           
+
+        }
+
+        public async Task<ActionResult> SendMessage(string message)
+        {
+            await queueClient.SendAsync(new Message(Encoding.UTF8.GetBytes(message)));
+            return Ok("Message added to the queue");
+        }
+
+
+        public ActionResult ReceiveMessage(string message)
+        {
+
+            var Options = new MessageHandlerOptions(EventException)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            queueClient.RegisterMessageHandler(EventMessage,Options);
+            return Ok("Message added to the queue");
+        }
+
+        public async Task EventMessage(Message message, CancellationToken token)
+        {
+            Encoding.UTF8.GetString(message.Body);
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        public Task EventException(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine(args.Exception);
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ Controllers/TopicController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TopicController: ControllerBase
+    {
+
+        private static string connectionString = "";
+        private static string topic = "";
+        private static string subscription = "";
+        public string eventMessage = "";
+        private ITopicClient topicClient;
+        private SubscriptionClient subscriptionClient;
+        public TopicController()
+        {
+            topicClient = new TopicClient(connectionString, topic);
+            //ServiceBusConnectionStringBuilder connectionStringBuilder = new ServiceBusConnectionStringBuilder(connectionString);
+            subscriptionClient = new SubscriptionClient(connectionString,topic, subscription);
+
+
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult> SendMessage(string message)
+        {
+            await topicClient.SendAsync(new Message(Encoding.UTF8.GetBytes(message)));
+            return Ok("Message added to the queue");
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult ReceiveMessage(string message)
+        {
+
+            var Options = new MessageHandlerOptions(EventException)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            subscriptionClient.RegisterMessageHandler(EventMessage, Options);
+            return Ok(eventMessage);
+        }
+
+        public async Task EventMessage(Message message, CancellationToken token)
+        {
+           eventMessage =  Encoding.UTF8.GetString(message.Body);
+
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+           
+        }
+
+        public Task EventException(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine(args.Exception);
+            return Task.CompletedTask;
+        }
+    }
+}
